62a77a048570945520099c9217241be1bf0792e9
Safronova Maryna <myjob.front.end@gmail.com>
2025-03-02 21:07:51 +0200

    add new settings

--- .amplify-hosting/compute/default/entry.mjs
+++ .amplify-hosting/compute/default/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BfVQpf87.mjs';
-import { manifest } from './manifest_B2asGjmd.mjs';
+import { manifest } from './manifest_CCfZcxry.mjs';
 
 const serverIslandMap = new Map();;
 
--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,6 @@ import awsAmplify from 'astro-aws-amplify';
 export default defineConfig({
   output: 'server',
   site: 'https://www.simbased.com',
-  image: {
-    service: passthroughImageService(),
-  },
   integrations: [tailwind({}), sitemap(), partytown({ config: { forward: ["dataLayer.push"] } }), robotsTxt({
     sitemap: true,
   })],
