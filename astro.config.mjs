846dd2893c3f32477ad3e13606f0ce10e837ab66
Safronova Maryna <myjob.front.end@gmail.com>
2025-03-02 22:10:49 +0200

    rollup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
