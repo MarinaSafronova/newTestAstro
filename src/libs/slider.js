fbeb5b03a763b848fbf98cb825348de4babfdabb
Safronova Maryna <myjob.front.end@gmail.com>
2025-03-03 21:02:26 +0200

    links

--- src/libs/slider.js
+++ src/libs/slider.js
@@ -1,28 +1,28 @@
 import "swiper/css";
-    import Swiper from "swiper";
-    import { Navigation } from 'swiper/modules';
-    (function () {
-        const swiper = new Swiper(".featured-slider", {
-            slidesPerView: "auto",
-            modules: [Navigation],
-            centeredSlides: true,
-            observer: true,
-            observeParents: true,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+(function () {
+    const swiper = new Swiper(".featured-slider", {
+        slidesPerView: "auto",
+        modules: [Navigation],
+        centeredSlides: true,
+        observer: true,
+        observeParents: true,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            300: {
+                slidesPerView: "auto",
+                centeredSlides: true,
+                spaceBetween: 20,
             },
-            breakpoints: {
-                300: {
-                    slidesPerView: "auto",
-                    centeredSlides: true,
-                    spaceBetween: 20,
-                },
-                580: {
-                    spaceBetween: 40,
-                    slidesPerView: "auto",
-                },
+            580: {
+                spaceBetween: 40,
+                slidesPerView: "auto",
             },
-        });
-    })();
+        },
+    });
+})();
